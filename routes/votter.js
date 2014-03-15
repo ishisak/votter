22079925a9280c04da38b97511323bf85d8cc618
Daisuke Watanabe <daigoro.white@gmail.com>
2014-03-15 11:25:50 +0900

    to ejs

--- routes/votter.js
+++ routes/votter.js
@@ -3,10 +3,14 @@
  * GET home page.
  */
 
-exports.top = function(req, res){
+ exports.top = function(req, res){
   //get top ** data from table
   //TODO I don't know how to access DB lol
-  res.render('top', { title: 'votter' });
+  
+  res.render('top', 
+    { title: 'votter',
+    events : [] 
+  });
 };
 
 exports.newform = function(req, res){
@@ -16,8 +20,14 @@ exports.newform = function(req, res){
 exports.create = function(req, res){
   //insert data to table
   //TODO I don't know how to access DB lol
+  var password = req.param("password")
+  var password2 = req.param("password2")
+  var title = req.param("title")
+  var detail = req.param("detail")
   res.redirect('/');
 };
+
+
 
 
 exports.vote = function(req, res){
@@ -26,6 +36,18 @@ exports.vote = function(req, res){
   //TODO I don't know how to access DB lol
   res.render('vote', { title: 'votter - vote now' });
 };
+
+exports.voted = function(req, res){
+  //get data from table
+  var vote_form_id = req.params.id;
+  var itle = req.param("elect");
+  var itle = req.param("comment");
+  //TODO I don't know how to access DB lol
+  res.redirect('/');
+};
+
+
+
 
 exports.mng = function(req, res){
   //get data from table
