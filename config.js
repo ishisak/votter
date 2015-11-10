26db7dd51a50d3324b2d2afc1b1bd646068e6eb9
Daisuke Watanabe <daigoro.white@gmail.com>
2015-11-10 22:55:20 +0900

    fix url of mongo 2

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 
-    'mongoUri': ' mongodb://votter:rakuten@ds031417.mongolab.com:31417/heroku_app23080744',
+    'mongoUri': 'mongodb://votter:rakuten@ds031417.mongolab.com:31417/heroku_app23080744',
     'dbOptions': {
 /*
 If you want any options, add option like the following.
